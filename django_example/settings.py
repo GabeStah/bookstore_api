bb3fd65d4fad50551dd901dbdd1667e58b7bda9e
Gabe Wyatt <gwyattkelsey@gmail.com>
2018-10-29 13:07:25 -0700

    Added ALLOWED_HOSTS

--- django_example/settings.py
+++ django_example/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-54-190-167-86.us-west-2.compute.amazonaws.com'
+]
 
 
 # Application definition
