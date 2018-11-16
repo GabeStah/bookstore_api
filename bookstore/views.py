fcf8b23d058c1178fcdf104dd3a0b23376af3879
Gabe Wyatt <gwyattkelsey@gmail.com>
2018-11-15 22:30:31 -0800

    Added 'get_version_timer'

--- bookstore/views.py
+++ bookstore/views.py
@@ -32,10 +32,10 @@ class BookViewSet(viewsets.ModelViewSet):
 
 @api_view()
 def get_version(request):
-    request.statsd.timings.start('get_version_timer:1|ms')
+    request.statsd.timings.start('get_version_timer')
 
     response = Response({'environment': config('ENVIRONMENT'), 'version': settings.VERSION})
-    request.statsd.timings.stop('get_version_timer:1|ms')
+    request.statsd.timings.stop('get_version_timer')
     return response
 
 # class UserViewSet(viewsets.ModelViewSet):
