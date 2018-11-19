8a014ee94ab1641d6265c644fa7377cd94cbb962
Gabe Wyatt <gwyattkelsey@gmail.com>
2018-11-19 03:41:54 -0800

    Changed metric dimensions to Host over Instance Id

--- metrics/cdn-connectivity.sh
+++ metrics/cdn-connectivity.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-EC2_INSTANCE_ID="$(ec2metadata --instance-id)"
+INSTANCE_ID="$(ec2metadata --instance-id)"
+TAG_NAME="$(aws ec2 describe-tags --filter "Name=resource-id,Values=$EC2_INSTANCE_ID" "Name=key,Values=Name" --query "Tags[*].Value" --output text)"
 
 while sleep 10
 do
     if nc -zv -w 5 cdn.bookstore.pingpublications.com 80 2>&1 | grep --line-buffered -i succeeded; then
-        /home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name cdn-connectivity --namespace Bookstore --dimensions Instance=$EC2_INSTANCE_ID --value 1
+        /home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name cdn-connectivity --namespace Bookstore --dimensions Host=$TAG_NAME --value 1
     else
         echo "Connection to cdn.bookstore.pingpublications.com 80 port [tcp/http] failed!"
-        /home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name cdn-connectivity --namespace Bookstore --dimensions Instance=$EC2_INSTANCE_ID --value 0
+        /home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name cdn-connectivity --namespace Bookstore --dimensions Host=$TAG_NAME --value 0
     fi
 done
--- metrics/db-connectivity.sh
+++ metrics/db-connectivity.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-EC2_INSTANCE_ID="$(ec2metadata --instance-id)"
+INSTANCE_ID="$(ec2metadata --instance-id)"
+TAG_NAME="$(aws ec2 describe-tags --filter "Name=resource-id,Values=$EC2_INSTANCE_ID" "Name=key,Values=Name" --query "Tags[*].Value" --output text)"
 
 while sleep 10
 do
     if nc -zv -w 5 db.bookstore.pingpublications.com 5432 2>&1 | grep --line-buffered -i succeeded; then
-        /home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name db-connectivity --namespace Bookstore --dimensions Instance=$EC2_INSTANCE_ID --value 1
+        /home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name db-connectivity --namespace Bookstore --dimensions Host=$TAG_NAME --value 1
     else
         echo "Connection to db.bookstore.pingpublications.com 5432 port [tcp/postgresql] failed!"
-        /home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name db-connectivity --namespace Bookstore --dimensions Instance=$EC2_INSTANCE_ID --value 0
+        /home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name db-connectivity --namespace Bookstore --dimensions Host=$TAG_NAME --value 0
     fi
 done
