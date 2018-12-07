96d22aa0900597f15c434204bedf1b60ae60dacd
Gabe Wyatt <gwyattkelsey@gmail.com>
2018-12-06 23:47:48 -0800

    bumped

--- api/settings.py
+++ api/settings.py
@@ -14,7 +14,7 @@ import os
 from decouple import config
 
 # Change version
-VERSION = 11
+VERSION = 12
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
